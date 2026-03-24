6e7b2a62b39777c257143aced5834eab32fc9f92
Lucas da Cunha <lucas.cunha0209@gmail.com>
2026-03-24 16:14:40 -0300

    ATUALIZACAO

--- src/views/HomePage.vue
+++ src/views/HomePage.vue
@@ -30,7 +30,6 @@ import router from '@/router';
 <style scoped>
 #container {
   text-align: center;
- 
   position: absolute;
   left: 0;
   right: 0;
@@ -47,7 +46,7 @@ import router from '@/router';
   font-size: 16px;
   line-height: 22px;
  
-  color: #00ffaa;
+  color: #8c8c8c;
  
   margin: 0;
 }
